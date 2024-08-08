37b62d48674fe05af7a2d79cef51ed949ed64530
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-08-08 12:06:26 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -15,8 +15,10 @@ char size(int cms) {
 
 int main() {
     assert(size(37) == 'S');
+    assert(size(38) == 'S');
     assert(size(38) == 'M');
     assert(size(40) == 'M');
+    assert(size(42) == 'M');
     assert(size(42) == 'L');
     assert(size(43) == 'L');
     printf("All is well (maybe!)\n");
